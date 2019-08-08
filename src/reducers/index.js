e67b29d928bd013f980f3fd76d733a3fb283dbfa
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-08 13:55:46 -0700

    Created songs and selectedSong reducers.

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: "Thriller - Michael Jackson", duration: "5:12" },
+    { title: "Like a Prayer - Madonna", duration: "5:42" },
+    { title: "When Doves Cry - Prince", duration: "5:52" },
+    { title: "I Wanna Dance with Somebody - Whitney Houston", duration: "4:51" }
+  ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload
+  }
+  return selectedSong
+}
