ab08756f69a4e247b45656e9bddc454c7a70a477
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-08 14:01:11 -0700

    Combined reducers and export default.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux"
+
 const songsReducer = () => {
   return [
     { title: "Thriller - Michael Jackson", duration: "5:12" },
@@ -13,3 +15,7 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
   return selectedSong
 }
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
