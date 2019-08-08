221696d57595987d3aec299238ef8ca652f8869c
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-08 14:38:44 -0700

    Used react-redux connect to map the state to props passed to the SongList component.

--- src/components/songList.js
+++ src/components/songList.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
 
 class SongList extends Component {
   render() {
+    console.log(this.props)
     return <h1>SongList</h1>
   }
 }
-export default SongList
+const mapStateToProps = state => {
+  return { songs: state.songs }
+}
+export default connect(mapStateToProps)(SongList)
